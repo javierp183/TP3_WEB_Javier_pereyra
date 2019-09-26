f18d2a1159f63c028636e3accd37a7b16c5c93c8
Javier Pereyra <javier.pereyra183@gmail.com>
2019-09-25 22:44:55 -0300

    initial commit

---
+++ Dominio/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Persona
+    {
+        public int Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+        public int Cp { get; set; }
+        public bool AceptoTerminos { get; set; }
+    }
+}
---
+++ TP3_Javier_Pereyra/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TP3_Javier_Pereyra.Global" Language="C#" %>
